875afea52b6e8f1c2188459a6f55de3a68eb1fd1
Tabasum khan <154969465+Tabbukhan@users.noreply.github.com>
2024-04-04 16:29:19 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
-
+	String username="user";
+	int password=123456;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
